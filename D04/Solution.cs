56a91c2cc90cafcff2d3a9510f1101dc9131be27
FaustVX <jodu035@gmail.com>
2025-10-29 19:25:01 +0100

    D04/1

--- D04/Solution.cs
+++ D04/Solution.cs
@@ -8,7 +8,15 @@ public sealed class Solution : ISolution
 {
     public string Solve1(ReadOnlySpan<char> input)
     {
-        throw new NotImplementedException();
+        var ranges = (stackalloc Range[input.Count('\n') + 1]);
+        var nails = (stackalloc int[ranges.Length]);
+        input.Split(ranges, '\n');
+        for (int i = 0; i < ranges.Length; i++)
+            nails[i] = int.Parse(input[ranges[i]]);
+        var min = nails.AsValueEnumerable().Min();
+        foreach (ref var nail in nails)
+            nail -= min;
+        return nails.AsValueEnumerable().Sum().ToString();
     }
 
     public string Solve2(ReadOnlySpan<char> input)
