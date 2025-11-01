b6a4f054e2954d13b7ff39c5fef7e26fd785cd78
FaustVX <jodu035@gmail.com>
2025-11-01 20:15:56 +0100

    Added `SpanIndexer` type to allow pattern-matching on `ROS<Ranges>` alloc-free

---
+++ EverybodyCodes.Core/Extensions/SpanIndexer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Diagnostics;
+
+namespace EverybodyCodes.Core.Extensions;
+
+[DebuggerStepThrough]
+public readonly ref struct SpanIndexer<T>(ReadOnlySpan<Range> ranges, ReadOnlySpan<T> line)
+{
+    private readonly ReadOnlySpan<Range> ranges = ranges;
+    private readonly ReadOnlySpan<T> line = line;
+
+    public int Length => ranges.Length;
+
+    public ReadOnlySpan<T> this[Index a] => line[ranges[a]];
+
+    public Enumerator GetEnumerator()
+    => new(this);
+
+    [DebuggerStepThrough]
+    public ref struct Enumerator(SpanIndexer<T> indexer) : IEnumerator<ReadOnlySpan<T>>
+    {
+        private int _index = 0;
+        private readonly SpanIndexer<T> indexer = indexer;
+
+        public readonly ReadOnlySpan<T> Current
+        => indexer[_index];
+
+        readonly object IEnumerator.Current
+        => Current.ToString();
+
+        public bool MoveNext()
+        => ++_index < indexer.Length;
+
+        readonly void IEnumerator.Reset() { }
+
+        readonly void IDisposable.Dispose() { }
+    }
+}
+
+[DebuggerStepThrough]
+public static partial class Extension
+{
+    extension(ReadOnlySpan<Range> span)
+    {
+        public SpanIndexer<T> ToIndexer<T>(ReadOnlySpan<T> line)
+        => new(span, line);
+    }
+}
