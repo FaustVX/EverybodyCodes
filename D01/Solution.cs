bdcebbce10f1a6e4a5878167da6802901f07131e
FaustVX <jodu035@gmail.com>
2025-11-03 21:24:40 +0100

    D01

---
+++ D01/Solution.cs
@@ -0,0 +1,23 @@
+using ZLinq;
+using EverybodyCodes.Core;
+
+namespace Y2.D01;
+
+// https://everybody.codes/story/2/quests/1
+public sealed class Solution : ISolution
+{
+    public string Solve1(ReadOnlySpan<char> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Solve2(ReadOnlySpan<char> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Solve3(ReadOnlySpan<char> input)
+    {
+        throw new NotImplementedException();
+    }
+}
