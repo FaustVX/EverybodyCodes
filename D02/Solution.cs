861a0b3b9904bb52fae1e75861e61f3320657734
FaustVX <jodu035@gmail.com>
2025-11-05 00:27:31 +0100

    D02/1

--- D02/Solution.cs
+++ D02/Solution.cs
@@ -10,7 +10,20 @@ public sealed class Solution : ISolution
 {
     public string Solve1(ReadOnlySpan<char> input)
     {
-        throw new NotImplementedException();
+        var ranges = (stackalloc Range[4]);
+        input.SplitAny(ranges, ['=', '[', ',', ']'], StringSplitOptions.RemoveEmptyEntries);
+
+        var a = new Complex(int.Parse(input[ranges[1]]), int.Parse(input[ranges[2]]));
+        var value = Complex.Zero;
+
+        for (var i = 0; i < 3; i++)
+        {
+            value *= value;
+            value /= new Complex(10, 10);
+            value += a;
+        }
+
+        return value.ToString();
     }
 
     public string Solve2(ReadOnlySpan<char> input)
@@ -22,4 +35,27 @@ public sealed class Solution : ISolution
     {
         throw new NotImplementedException();
     }
+}
+
+file record struct Complex(int Real, int Imaginary)
+{
+    public static Complex Zero => new();
+
+    public void operator +=(Complex other)
+    {
+        Real += other.Real;
+        Imaginary += other.Imaginary;
+    }
+
+    public void operator *=(Complex other)
+    => (Real, Imaginary) = (Real * other.Real - Imaginary * other.Imaginary, Real * other.Imaginary + Imaginary * other.Real);
+
+    public void operator /=(Complex other)
+    {
+        Real /= other.Real;
+        Imaginary /= other.Imaginary;
+    }
+
+    public override readonly string ToString()
+    => $"[{Real},{Imaginary}]";
 }
