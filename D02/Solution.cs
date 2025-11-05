9669a29457f82f734ffabec3624c6bd3345f01c4
FaustVX <jodu035@gmail.com>
2025-11-05 01:27:39 +0100

    Extracted common logic for p1&2

--- D02/Solution.cs
+++ D02/Solution.cs
@@ -1,7 +1,5 @@
-using ZLinq;
 using CommunityToolkit.HighPerformance;
 using EverybodyCodes.Core;
-using EverybodyCodes.Core.Extensions;
 
 namespace Y2025.D02;
 
@@ -10,10 +8,7 @@ public sealed class Solution : ISolution
 {
     public string Solve1(ReadOnlySpan<char> input)
     {
-        var ranges = (stackalloc Range[4]);
-        input.SplitAny(ranges, ['=', '[', ',', ']'], StringSplitOptions.RemoveEmptyEntries);
-
-        var a = new Complex(int.Parse(input[ranges[1]]), int.Parse(input[ranges[2]]));
+        var a = Complex.Parse(input[input.IndexOf('[')..]);
         var value = Complex.Zero;
 
         for (var i = 0; i < 3; i++)
@@ -27,50 +22,22 @@ public sealed class Solution : ISolution
     }
 
     public string Solve2(ReadOnlySpan<char> input)
-    {
-        var ranges = (stackalloc Range[4]);
-        input.SplitAny(ranges, ['=', '[', ',', ']'], StringSplitOptions.RemoveEmptyEntries);
-
-        var a = new Complex(int.Parse(input[ranges[1]]), int.Parse(input[ranges[2]]));
-        var count = 0;
-
-        for (var y = 0; y < 101; y++)
-            for (var x = 0; x < 101; x++)
-            {
-                var isValid = true;
-                var p = a + new Complex(x * 10, y * 10);
-                var result = p;
-                for (var i = 1; i < 100; i++)
-                {
-                    result *= result;
-                    result /= new Complex(100000, 100000);
-                    result += p;
-                    if (Math.Abs(result.Real) > 1000000 || Math.Abs(result.Imaginary) > 1000000)
-                    {
-                        isValid = false;
-                        break;
-                    }
-                }
-                if (isValid)
-                    count++;
-            }
-
-        return count.ToString();
-    }
+    => Solve(input, 101).ToString();
 
     public string Solve3(ReadOnlySpan<char> input)
+    => Solve(input, 1001).ToString();
+
+    static int Solve(ReadOnlySpan<char> input, int size)
     {
-        var ranges = (stackalloc Range[4]);
-        input.SplitAny(ranges, ['=', '[', ',', ']'], StringSplitOptions.RemoveEmptyEntries);
-
-        var a = new Complex(int.Parse(input[ranges[1]]), int.Parse(input[ranges[2]]));
+        var a = Complex.Parse(input[input.IndexOf('[')..]);
         var count = 0;
+        var offset = 1000 / (size - 1);
 
-        for (var y = 0; y < 1001; y++)
-            for (var x = 0; x < 1001; x++)
+        for (var y = 0; y < size; y++)
+            for (var x = 0; x < size; x++)
             {
                 var isValid = true;
-                var p = a + new Complex(x, y);
+                var p = a + new Complex(x * offset, y * offset);
                 var result = p;
                 for (var i = 1; i < 100; i++)
                 {
@@ -87,13 +54,20 @@ public sealed class Solution : ISolution
                     count++;
             }
 
-        return count.ToString();
+        return count;
     }
 }
 
 file record struct Complex(long Real, long Imaginary)
 {
     public static Complex Zero => new();
+
+    public static Complex Parse(ReadOnlySpan<char> input)
+    {
+        input = input[1..^1];
+        var comma = input.IndexOf(',');
+        return new(int.Parse(input[..comma]), int.Parse(input[(comma+1)..]));
+    }
 
     public void operator +=(Complex other)
     {
