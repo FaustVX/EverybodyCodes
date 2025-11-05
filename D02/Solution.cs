1c86209fc66762885d2f80add1ab2b8c7e6ab682
FaustVX <jodu035@gmail.com>
2025-11-05 01:11:13 +0100

    D02/2

--- D02/Solution.cs
+++ D02/Solution.cs
@@ -28,7 +28,34 @@ public sealed class Solution : ISolution
 
     public string Solve2(ReadOnlySpan<char> input)
     {
-        throw new NotImplementedException();
+        var ranges = (stackalloc Range[4]);
+        input.SplitAny(ranges, ['=', '[', ',', ']'], StringSplitOptions.RemoveEmptyEntries);
+
+        var a = new Complex(int.Parse(input[ranges[1]]), int.Parse(input[ranges[2]]));
+        var count = 0;
+
+        for (var y = 0; y < 101; y++)
+            for (var x = 0; x < 101; x++)
+            {
+                var isValid = true;
+                var p = a + new Complex(x * 10, y * 10);
+                var result = p;
+                for (var i = 1; i < 100; i++)
+                {
+                    result *= result;
+                    result /= new Complex(100000, 100000);
+                    result += p;
+                    if (Math.Abs(result.Real) > 1000000 || Math.Abs(result.Imaginary) > 1000000)
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+                if (isValid)
+                    count++;
+            }
+
+        return count.ToString();
     }
 
     public string Solve3(ReadOnlySpan<char> input)
@@ -37,7 +64,7 @@ public sealed class Solution : ISolution
     }
 }
 
-file record struct Complex(int Real, int Imaginary)
+file record struct Complex(long Real, long Imaginary)
 {
     public static Complex Zero => new();
 
@@ -47,14 +74,23 @@ file record struct Complex(int Real, int Imaginary)
         Imaginary += other.Imaginary;
     }
 
+    public static Complex operator +(Complex left, Complex right)
+    => left += right;
+
     public void operator *=(Complex other)
     => (Real, Imaginary) = (Real * other.Real - Imaginary * other.Imaginary, Real * other.Imaginary + Imaginary * other.Real);
+
+    public static Complex operator *(Complex left, Complex right)
+    => left *= right;
 
     public void operator /=(Complex other)
     {
         Real /= other.Real;
         Imaginary /= other.Imaginary;
     }
+
+    public static Complex operator /(Complex left, Complex right)
+    => left /= right;
 
     public override readonly string ToString()
     => $"[{Real},{Imaginary}]";
