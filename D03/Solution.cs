38e1f0b24276dc4e867e64226ba4cb9256edbd3c
FaustVX <jodu035@gmail.com>
2025-11-06 00:10:51 +0100

    D03/2

--- D03/Solution.cs
+++ D03/Solution.cs
@@ -13,12 +13,21 @@ public sealed class Solution : ISolution
         var set = new HashSet<int>();
         foreach (var number in input.Split(','))
             set.Add(int.Parse(input[number]));
-        return set.Sum().ToString();
+        return set.AsValueEnumerable()
+            .Sum()
+            .ToString();
     }
 
     public string Solve2(ReadOnlySpan<char> input)
     {
-        throw new NotImplementedException();
+        var set = new HashSet<int>();
+        foreach (var number in input.Split(','))
+            set.Add(int.Parse(input[number]));
+        return set.AsValueEnumerable()
+            .Order()
+            .Take(20)
+            .Sum()
+            .ToString();
     }
 
     public string Solve3(ReadOnlySpan<char> input)
