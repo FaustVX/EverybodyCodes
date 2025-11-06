1b9800ed056f673fccdbc1d663fe911ba1faf733
FaustVX <jodu035@gmail.com>
2025-11-06 23:09:46 +0100

    D02/1

--- D02/Solution.cs
+++ D02/Solution.cs
@@ -2,6 +2,7 @@ using ZLinq;
 using CommunityToolkit.HighPerformance;
 using EverybodyCodes.Core;
 using EverybodyCodes.Core.Extensions;
+using System.Diagnostics;
 
 namespace Y2.D02;
 
@@ -10,7 +11,18 @@ public sealed class Solution : ISolution
 {
     public string Solve1(ReadOnlySpan<char> input)
     {
-        throw new NotImplementedException();
+        for (var i = 0;; i++)
+        {
+            input = (i % 3) switch
+            {
+                0 => input.TrimStart('R'),
+                1 => input.TrimStart('G'),
+                2 => input.TrimStart('B'),
+                _ => throw new UnreachableException(),
+            };
+            if (input.IsEmpty || (input = input[1..]).IsEmpty)
+                return (i + 1).ToString();
+        }
     }
 
     public string Solve2(ReadOnlySpan<char> input)
