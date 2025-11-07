8536162e2e651e7f9b56e3bed04f3e911f0fb07a
FaustVX <jodu035@gmail.com>
2025-11-08 00:27:25 +0100

    D05/2

--- D05/Solution.cs
+++ D05/Solution.cs
@@ -9,22 +9,34 @@ namespace Y2025.D05;
 public sealed class Solution : ISolution
 {
     public string Solve1(ReadOnlySpan<char> input)
-    {
-        input = input[(input.IndexOf(':') + 1)..];
-        var spine = new Segment(int.Parse(input[..input.IndexOf(',')]));
-        foreach (var r in input.Split(',').Skip())
-            spine.Add(int.Parse(input[r]));
-        return spine.ToString();
-    }
+    => GetSwordQuality(input).ToString();
 
     public string Solve2(ReadOnlySpan<char> input)
     {
-        throw new NotImplementedException();
+        var (min, max) = (long.MaxValue, long.MinValue);
+        foreach (var line in input.Split('\n'))
+        {
+            var quality = GetSwordQuality(input[line]);
+            if (quality < min)
+                min = quality;
+            if (quality > max)
+                max = quality;
+        }
+        return (max - min).ToString();
     }
 
     public string Solve3(ReadOnlySpan<char> input)
     {
         throw new NotImplementedException();
+    }
+
+    static long GetSwordQuality(ReadOnlySpan<char> input)
+    {
+        input = input[(input.IndexOf(':') + 1)..];
+        var spine = new Segment(int.Parse(input[..input.IndexOf(',')]));
+        foreach (var r in input.Split(',').Skip())
+            spine.Add(int.Parse(input[r]));
+        return long.Parse(spine.ToString());
     }
 }
 
