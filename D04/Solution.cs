e0961cd8406c349276a95275a7af6e7b36c98968
FaustVX <jodu035@gmail.com>
2025-11-07 21:58:20 +0100

    D04/2

--- D04/Solution.cs
+++ D04/Solution.cs
@@ -17,7 +17,9 @@ public sealed class Solution : ISolution
 
     public string Solve2(ReadOnlySpan<char> input)
     {
-        throw new NotImplementedException();
+        var first = double.Parse(input[..input.IndexOf('\n')]);
+        var last = double.Parse(input[(input.LastIndexOf('\n') + 1)..]);
+        return Math.Ceiling(10000000000000.0 / (first / last)).ToString();
     }
 
     public string Solve3(ReadOnlySpan<char> input)
