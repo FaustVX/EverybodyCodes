c94f886a6dea95097e29cee41034799859f2feff
FaustVX <jodu035@gmail.com>
2025-11-08 01:50:28 +0100

    D04/3

--- D04/Solution.cs
+++ D04/Solution.cs
@@ -2,6 +2,7 @@ using ZLinq;
 using CommunityToolkit.HighPerformance;
 using EverybodyCodes.Core;
 using EverybodyCodes.Core.Extensions;
+using System.Diagnostics;
 
 namespace Y2025.D04;
 
@@ -17,13 +18,42 @@ public sealed class Solution : ISolution
 
     public string Solve2(ReadOnlySpan<char> input)
     {
-        var first = double.Parse(input[..input.IndexOf('\n')]);
-        var last = double.Parse(input[(input.LastIndexOf('\n') + 1)..]);
-        return Math.Ceiling(10000000000000.0 / (first / last)).ToString();
+        var first = decimal.Parse(input[..input.IndexOf('\n')]);
+        var last = decimal.Parse(input[(input.LastIndexOf('\n') + 1)..]);
+        return Math.Ceiling(1e13m / (first / last)).ToString();
     }
 
     public string Solve3(ReadOnlySpan<char> input)
     {
-        throw new NotImplementedException();
+        var previous = decimal.Parse(input[..input.IndexOf('\n')]);
+        var ratio = 1m;
+        foreach (var range in input.Split('\n').Skip())
+        {
+            var gear = input[range];
+            if (gear.IndexOf('|') is var pipe and not < 0)
+            {
+                var left = decimal.Parse(gear[..pipe]);
+                ratio /= left / previous;
+                previous = decimal.Parse(gear[(pipe + 1)..]);
+            }
+            else
+                ratio /= decimal.Parse(gear) / previous;
+        }
+        return ((long)(ratio * 100)).ToString();
+    }
+}
+
+file static class Ext
+{
+    extension<T>(System.MemoryExtensions.SpanSplitEnumerator<T> enumerator)
+    where T : IEquatable<T>
+    {
+        public System.MemoryExtensions.SpanSplitEnumerator<T> Skip(int count = 1)
+        {
+            enumerator = enumerator.GetEnumerator();
+            for (var i = 0; i < count; i++)
+                enumerator.MoveNext();
+            return enumerator;
+        }
     }
 }
