b3dad3481cc2a64b23eff794cfe88729dd187202
FaustVX <jodu035@gmail.com>
2025-11-08 22:20:10 +0100

    D03/1

--- D03/Solution.cs
+++ D03/Solution.cs
@@ -2,15 +2,31 @@ using ZLinq;
 using CommunityToolkit.HighPerformance;
 using EverybodyCodes.Core;
 using EverybodyCodes.Core.Extensions;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
 
 namespace Y2.D03;
+using System;
+using System.Linq;
 
 // https://everybody.codes/story/2/quests/3
 public sealed class Solution : ISolution
 {
     public string Solve1(ReadOnlySpan<char> input)
     {
-        throw new NotImplementedException();
+        var dices = (stackalloc Die[input.Count('\n') + 1]);
+        var i = 0;
+        foreach (var die in input.Split('\n'))
+            dices[i++] = Die.Parse(input[die]);
+        var points = 10_000;
+        for (i = 0; points > 0; i++)
+        {
+            var score = 0;
+            foreach (ref var die in dices)
+                score += die.Roll();
+            points -= score;
+        }
+        return i.ToString();
     }
 
     public string Solve2(ReadOnlySpan<char> input)
@@ -22,4 +38,54 @@ public sealed class Solution : ISolution
     {
         throw new NotImplementedException();
     }
+}
+
+file struct Die(int index, Array faces, int facesCount, int seed)
+{
+    private readonly Array faces = faces;
+    public readonly int Index { get; } = index;
+    public readonly int Seed { get; } = seed;
+    public readonly int _facesCount = facesCount;
+    private int _pulse = seed;
+    private int _rollCount = 1;
+    private int _lastRolled = 0;
+
+    public int Roll()
+    {
+        var spin = _rollCount * _pulse;
+        _lastRolled = (spin + _lastRolled) % _facesCount;
+        var roll = faces[_lastRolled];
+        _pulse += spin;
+        _pulse %= Seed;
+        _pulse += 1 + _rollCount + Seed;
+        _rollCount++;
+        return roll;
+    }
+
+    public static Die Parse(ReadOnlySpan<char> input)
+    {
+        var ranges = (stackalloc Range[5]);
+        input.SplitAny(ranges, [' ', '=', ':'], StringSplitOptions.RemoveEmptyEntries);
+        if (ranges.ToIndexer(input) is [var index, _, ['[', .. var faces, ']'], _, var seed])
+            return new(int.Parse(index), new(faces.Split(',')), faces.Count(',') + 1, int.Parse(seed));
+        throw new UnreachableException();
+    }
+}
+
+[InlineArray(10)]
+file struct Array
+{
+    public Array(ReadOnlySpan<int> ints)
+    => ints.CopyTo(this);
+    public Array(MemoryExtensions.SpanSplitEnumerator<char> enumerator)
+    {
+        Span<int> array = this;
+        foreach (var item in enumerator)
+        {
+            array[0] = int.Parse(enumerator.Source[item]);
+            array = array[1..];
+        }
+    }
+
+    private int _field;
 }
