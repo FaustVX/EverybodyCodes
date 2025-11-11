1870929bb4c1137ba4578109e4568a5ecfdbfbf1
FaustVX <jodu035@gmail.com>
2025-11-12 00:23:53 +0100

    D07/1

--- D07/Solution.cs
+++ D07/Solution.cs
@@ -1,5 +1,4 @@
 using ZLinq;
-using CommunityToolkit.HighPerformance;
 using EverybodyCodes.Core;
 using EverybodyCodes.Core.Extensions;
 
@@ -11,6 +10,22 @@ public sealed class Solution : ISolution
     // https://everybody.codes/event/2025/quests/7#:~:text=Part%20I
     public string Solve1(ReadOnlySpan<char> input)
     {
+        var nl = input.IndexOf("\n\n");
+        var names = input[..nl];
+        var rules = CreateRules(input[(nl + 2)..]);
+        foreach (var nameR in names.Split(','))
+        {
+            var name = names[nameR];
+            var found = true;
+            for (var i = 1; i < name.Length; i++)
+                if (!rules[name[i - 1]].Contains(name[i]))
+                {
+                    found = false;
+                    break;
+                }
+            if (found)
+                return name.ToString();
+        }
         throw new NotImplementedException();
     }
 
@@ -24,5 +39,19 @@ public sealed class Solution : ISolution
     public string Solve3(ReadOnlySpan<char> input)
     {
         throw new NotImplementedException();
+    }
+
+    static Dictionary<char, HashSet<char>> CreateRules(ReadOnlySpan<char> input)
+    {
+        var rules = new Dictionary<char, HashSet<char>>(capacity: input.Count('\n') + 1);
+        foreach (var line in input.Split('\n'))
+        {
+            var set = new HashSet<char>(capacity: input[line].Count(',') + 1);
+            foreach (var c in input[line][4..])
+                if (c is not ',')
+                    set.Add(c);
+            rules.Add(input[line][0], set);
+        }
+        return rules;
     }
 }
