d79535935aeda30a17d9fedcf06b2e5591f251ef
FaustVX <jodu035@gmail.com>
2025-11-12 01:19:58 +0100

    D07/3

--- D07/Solution.cs
+++ D07/Solution.cs
@@ -1,6 +1,6 @@
-using ZLinq;
+using System.Collections.Immutable;
 using EverybodyCodes.Core;
-using EverybodyCodes.Core.Extensions;
+using ZLinq;
 
 namespace Y2025.D07;
 
@@ -42,16 +42,40 @@ public sealed class Solution : ISolution
         var nl = input.IndexOf("\n\n");
         var names = input[..nl];
         var rules = CreateRules(input[(nl + 2)..]);
-        var sum = 0;
+        var set = new HashSet<string>(capacity: 10_000);
         foreach (var nameR in names.Split(','))
-        {
             if (IsValidName(rules, names[nameR]))
-                sum += i;
+                GenerateNames(names[nameR].ToImmutableArray(), set, rules);
+        return set.Count.ToString();
+
+        static int GenerateNames(ImmutableArray<char> name, HashSet<string> names, IReadOnlyDictionary<char, IReadOnlySet<char>> rules)
+        {
+            if (name.Length < 11)
+                foreach (var c in rules.TryGet(name[^1]) ?? _empty)
+                {
+                    if (name.Length >= 6)
+                        names.Add(CreateName(name, c));
+                    GenerateNames(name.Add(c), names, rules);
+                }
+
+            return names.Count;
+
+            static string CreateName(ImmutableArray<char> name, char suffix)
+            => string.Create(name.Length + 1, (name, suffix), (s, i) =>
+            {
+                foreach (var c in i.name)
+                {
+                    s[0] = c;
+                    s = s[1..];
+                }
+                s[0] = i.suffix;
+            });
         }
-        return sum.ToString();
     }
 
-    static bool IsValidName(IReadOnlyDictionary<char, ISet<char>> rules, ReadOnlySpan<char> name)
+    static readonly IReadOnlySet<char> _empty = new HashSet<char>(capacity: 0);
+
+    static bool IsValidName(IReadOnlyDictionary<char, IReadOnlySet<char>> rules, ReadOnlySpan<char> name)
     {
         var found = true;
         for (var i = 1; i < name.Length; i++)
@@ -65,9 +89,9 @@ public sealed class Solution : ISolution
         return false;
     }
 
-    static IReadOnlyDictionary<char, ISet<char>> CreateRules(ReadOnlySpan<char> input)
+    static IReadOnlyDictionary<char, IReadOnlySet<char>> CreateRules(ReadOnlySpan<char> input)
     {
-        var rules = new Dictionary<char, ISet<char>>(capacity: input.Count('\n') + 1);
+        var rules = new Dictionary<char, IReadOnlySet<char>>(capacity: input.Count('\n') + 1);
         foreach (var line in input.Split('\n'))
         {
             var set = new HashSet<char>(capacity: input[line].Count(',') + 1);
@@ -77,5 +101,16 @@ public sealed class Solution : ISolution
             rules.Add(input[line][0], set);
         }
         return rules;
+    }
+}
+
+file static class Ext
+{
+    extension<TKey, TValue>(IReadOnlyDictionary<TKey, TValue> dict)
+    {
+        public TValue? TryGet(TKey key)
+        {
+            return CollectionExtensions.GetValueOrDefault(dict, key);
+        }
     }
 }
