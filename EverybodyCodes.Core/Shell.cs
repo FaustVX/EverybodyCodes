96978cd6b755fa6c2c2d362be22034601605f63d
FaustVX <jodu035@gmail.com>
2025-11-22 18:08:00 +0100

    Fixed `Shell.Expand` to eliminate empty strings

--- EverybodyCodes.Core/Shell.cs
+++ EverybodyCodes.Core/Shell.cs
@@ -29,21 +29,21 @@ public static class Shell
         }
     }
 
-    private static string Expand(bool b, string @true = "", string @false = "")
-    => b ? @true : @false;
+    private static string[] Expand(bool b, string @true = "", string @false = "")
+    => (b ? @true : @false) is var s && !string.IsNullOrWhiteSpace(s) ? [s] : Array.Empty<string>();
 
-    private static string Expand(bool? b, string @true = "", string @false = "", string @null = "")
-    => b switch
+    private static string[] Expand(bool? b, string @true = "", string @false = "", string @null = "")
+    => (b switch
     {
         true => @true,
         false => @false,
         null => @null,
-    };
+    }) is var s && !string.IsNullOrWhiteSpace(s) ? [s] : Array.Empty<string>();
 
     public static class Git
     {
         public static Task Commit(string message, bool quiet = true, params ReadOnlySpan<string> args)
-        => StartAsync("git", ["commit", Expand(quiet, "--quiet"), "-m", message, ..args]);
+        => StartAsync("git", ["commit", ..Expand(quiet, "--quiet"), "-m", message, ..args]);
         public static Task Add(params ReadOnlySpan<string> args)
         => StartAsync("git", ["add", ..args]);
         public static Task Init(params ReadOnlySpan<string> args)
@@ -54,25 +54,25 @@ public static class Shell
             public static Task Add(string repo, string folder, string? branch = null, params ReadOnlySpan<string> args)
             => StartAsync("git", ["submodule", "add", ..branch is not null ? ["-b", branch] : Array.Empty<string>(), ..args, "--", repo, folder]);
             public static Task Update(bool init = false, bool recursive = false, params ReadOnlySpan<string> args)
-            => StartAsync("git", ["submodule", "update", Expand(init, "--init"), Expand(recursive, "--recursive"), ..args]);
+            => StartAsync("git", ["submodule", "update", ..Expand(init, "--init"), ..Expand(recursive, "--recursive"), ..args]);
         }
 
         public static class Worktree
         {
             public static Task Add(string? branch, string folder, bool orphan = false, params ReadOnlySpan<string> args)
-            => StartAsync("git", ["worktree", "add", ..branch is string b ? ["-b", branch] : Array.Empty<string>(), Expand(orphan, "--orphan"), folder, ..args]);
+            => StartAsync("git", ["worktree", "add", ..branch is string b ? ["-b", branch] : Array.Empty<string>(), ..Expand(orphan, "--orphan"), folder, ..args]);
         }
         public static Task Checkout(string branch, bool create = false, bool quiet = true, params ReadOnlySpan<string> args)
-        => StartAsync("git", ["checkout", Expand(quiet, "--quiet"), ..args, Expand(create, "-b"), branch]);
+        => StartAsync("git", ["checkout", ..Expand(quiet, "--quiet"), ..args, ..Expand(create, "-b"), branch]);
 
         public static Task Merge(ReadOnlySpan<string> branches, bool? ff = null, bool quiet = true, params ReadOnlySpan<string> args)
-        => StartAsync("git", ["merge", Expand(quiet, "--quiet"), ..args, Expand(ff, "--ff", "--no-ff"), ..branches]);
+        => StartAsync("git", ["merge", ..Expand(quiet, "--quiet"), ..args, ..Expand(ff, "--ff", "--no-ff"), ..branches]);
 
         public static Task Merge(string branch, bool? ff = null, bool quiet = true, params ReadOnlySpan<string> args)
         => Merge([branch], ff, quiet, args);
 
         public static Task<string> RevParse(string @ref, bool abbrevRef = false)
-        => StartOutputAsync("git", ["rev-parse", Expand(abbrevRef, "--abbrev-ref"), @ref]);
+        => StartOutputAsync("git", ["rev-parse", ..Expand(abbrevRef, "--abbrev-ref"), @ref]);
     }
 
     public static class Dotnet
@@ -96,9 +96,9 @@ public static class Shell
         private static readonly string? profile = "C#";
 
         public static Task OpenInExistingWindow(IEnumerable<string> files, bool wait = false, params IEnumerable<string> args)
-        => StartAsync(code, [..profile is not null ? ["--profile", profile] : Array.Empty<string>(), "--reuse-window", Expand(wait, "--wait"), ..args, "--", ..files]);
+        => StartAsync(code, [..profile is not null ? ["--profile", profile] : Array.Empty<string>(), "--reuse-window", ..Expand(wait, "--wait"), ..args, "--", ..files]);
 
         public static Task OpenInNewWindow(IEnumerable<string> files, bool wait = false, params IEnumerable<string> args)
-        => StartAsync(code, [..profile is not null ? ["--profile", profile] : Array.Empty<string>(), "--new-window", Expand(wait, "--wait"), ..args, "--", ..files]);
+        => StartAsync(code, [..profile is not null ? ["--profile", profile] : Array.Empty<string>(), "--new-window", ..Expand(wait, "--wait"), ..args, "--", ..files]);
     }
 }
