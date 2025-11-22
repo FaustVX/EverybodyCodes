c90ca621346de36c6d336095d4b3bf93f62674ff
FaustVX <jodu035@gmail.com>
2025-11-22 15:20:58 +0100

    D11

---
+++ D11/Solution.cs
@@ -0,0 +1,28 @@
+using ZLinq;
+using CommunityToolkit.HighPerformance;
+using EverybodyCodes.Core;
+using EverybodyCodes.Core.Extensions;
+
+namespace Y2025.D11;
+
+// https://everybody.codes/event/2025/quests/11
+public sealed class Solution : ISolution
+{
+    // https://everybody.codes/event/2025/quests/11#:~:text=Part%20I
+    public string Solve1(ReadOnlySpan<char> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    // https://everybody.codes/event/2025/quests/11#:~:text=Part%20II
+    public string Solve2(ReadOnlySpan<char> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    // https://everybody.codes/event/2025/quests/11#:~:text=Part%20III
+    public string Solve3(ReadOnlySpan<char> input)
+    {
+        throw new NotImplementedException();
+    }
+}
